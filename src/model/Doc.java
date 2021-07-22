6fde6e63cf47777a0bd7da4ded450f06f4cfad83
Ivo Melo <ivo.melo.988@gmail.com>
2021-07-22 00:50:47 -0300

    change Doc parameters

--- src/model/Doc.java
+++ src/model/Doc.java
@@ -1,20 +1,18 @@
 package model;
 
-import javax.swing.JTextField;
-
 public class Doc {
 	
-	private String nome						= "";
-	private String cpf						= "";
-	private String especialidade	= "";
-	private String crm						= "";
+	private String nome;
+	private String cpf;
+	private String especialidade;
+	private String crm;
 	
-	public Doc(JTextField nome, JTextField cpf, JTextField especialidade, JTextField crm) {
+	public Doc(String nome, String cpf, String especialidade, String crm) {
 		
-		this.nome 					= nome.getText();
-		this.cpf						= cpf.getText();
-		this.especialidade	= especialidade.getText();
-		this.crm						= crm.getText();
+		this.nome 					= nome;
+		this.cpf						= cpf;
+		this.especialidade	= especialidade;
+		this.crm						= crm;
 		
 	}	
 	
