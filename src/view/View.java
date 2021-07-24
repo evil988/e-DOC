6ba9c969e665b9c77e0290d0250291f17fdd4683
Ivo Melo <ivo.melo.988@gmail.com>
2021-07-24 01:39:06 -0300

    add View interface

---
+++ src/view/View.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.JFrame;
+
+public interface View {
+	
+	public void initialize(JFrame mw);
+	public void initComponents();
+	public void display(JFrame mw);
+	
+}
