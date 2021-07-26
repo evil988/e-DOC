f261f3b80917ea77b8255be6749ca17cd38f04b4
Ivo Melo <ivo.melo.988@gmail.com>
2021-07-26 17:19:11 -0300

    add DocList

---
+++ src/model/DocList.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+
+public class DocList implements Dao<Doc, String[][]>{
+	private ArrayList<Doc> doclist;
+	private static DocList dl;
+	
+	private DocList() {
+		doclist = new ArrayList<Doc>();
+	}
+	
+	public static DocList getInstance() {
+		if (dl == null)
+			dl = new DocList();
+		return dl;
+	}
+
+	@Override
+	public void add(Doc obj) {
+		doclist.add(obj);
+	}
+
+	@Override
+	public String[][] show() {
+		String[][] rows = new String[doclist.size()][4];
+		int i = 0;
+		for (Doc doc : doclist) {
+			rows[i][0] = doc.getNome();
+			rows[i][1] = doc.getCpf();
+			rows[i][2] = doc.getEspecialidade();
+			rows[i][3] = doc.getCrm();
+			i++;
+		}
+		return rows;
+	}
+}
