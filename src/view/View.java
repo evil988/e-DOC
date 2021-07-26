cd959b2a8b62ea5a5f594789af1baeb0df9e66c9
Ivo Melo <ivo.melo.988@gmail.com>
2021-07-26 19:37:57 -0300

    change parameters type

--- src/view/View.java
+++ src/view/View.java
@@ -1,11 +1,9 @@
 package view;
 
-import javax.swing.JFrame;
-
 public interface View {
 	
-	public void initialize(JFrame mw);
+	public void initialize(Object mw);
 	public void initComponents();
-	public void display(JFrame mw);
+	public void display();
 	
 }
