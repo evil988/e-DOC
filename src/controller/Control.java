843dd435eb49535a75808e85db1434f288989b46
Ivo Melo <ivo.melo.988@gmail.com>
2021-07-27 16:14:01 -0300

    feat unregister observer

--- src/controller/Control.java
+++ src/controller/Control.java
@@ -3,7 +3,8 @@ package controller;
 public interface Control {
 	public void save(String values[]);
 	public String[][] tabRows();
-	public void RegView(Object mw);
-	public void TabView(Object mw);
-	public void RecObs(Observer obs);
+	public void regView(Object mw);
+	public void tabView(Object mw);
+	public void recObs(Observer o);
+	public void unRegObs(Observer o);
 }
