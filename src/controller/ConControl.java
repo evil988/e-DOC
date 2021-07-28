413cdb1f882e6521f76df590a5b71a2e217ab8bd
Ivo Melo <ivo.melo.988@gmail.com>
2021-07-28 18:39:37 -0300

    feat: save function

--- src/controller/ConControl.java
+++ src/controller/ConControl.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.ConList;
+import model.Consultation;
 import view.ConTable;
 import view.NewCon;
 
@@ -17,8 +19,7 @@ public class ConControl implements Control{
 
 	@Override
 	public void save(String[] values) {
-		// TODO Auto-generated method stub
-		
+		ConList.getInstance().add(new Consultation(values[0], values[1], values[2], values[3]));		
 	}
 
 	@Override
